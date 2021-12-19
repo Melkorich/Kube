4b203347857aa4f639e07fceb1a48dfe9062dfc6
Melkorich <Kirill.Lavrov92@gmail.com>
2021-12-19 19:34:22 +0300

    reputation section finished and shows section added

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,13 @@
 $(function(){
+
+  $('.album-slider').slick({
+
+    vertical: true,
+    slidesToShow:3,
+    verticalSwiping: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
  
 });
